903e8dbe334e731e1707b2eb484079080c71c14a
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2023-06-23 11:44:10 +0545

    Updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
-import "./App.css";
+import "./App.css"; // 19:17
 
 const App = () => {
-  const arr = [1, 2, 3, 4, 5];
+  const arr = [
+    {
+      id: 1,
+      name: 'a',
+    },
+    {
+      id: 2,
+      name: 'b',
+    }
+  ];
 
   return (
     <div className="App">
@@ -11,7 +20,13 @@ const App = () => {
         //   <div>{num} ,</div>
         // ))
 
-        arr.filter((num) => num !== 3 ) // continue from 13:57 (https://www.youtube.com/watch?v=PGlnnr22qj0&t=461s)
+        // arr.filter((num) => num !== 3 )
+
+        arr.map((num) => {
+          return (
+            <div key={num.id}>{num.name}</div>
+          )
+        })
       }
     </div>
   )
