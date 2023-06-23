573d782b369a242daac808dd5389fd174f78563e
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2023-06-23 11:09:20 +0545

    Updated

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ const App = () => {
   return (
     <div className="App">
       {
-        arr.map((num) => (
-          <div>{num}</div>
-        ))
+        // arr.map((num) => (
+        //   <div>{num} ,</div>
+        // ))
+
+        arr.filter((num) => num !== 3 ) // continue from 13:57 (https://www.youtube.com/watch?v=PGlnnr22qj0&t=461s)
       }
     </div>
   )
