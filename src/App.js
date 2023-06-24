3f4fae1b9e2bf336757c68fac6c2041347cf1977
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2023-06-24 12:06:06 +0545

    Updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from "react";
-import "./App.css"; // 19:17
+import React, { useState } from "react";
+import "./App.css";
 
 const App = () => {
-  const arr = [
-    {
-      id: 1,
-      name: 'a',
-    },
-    {
-      id: 2,
-      name: 'b',
+  // const arr = [
+  //   {
+  //     id: 1,
+  //     name: 'a',
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'b',
+  //   }
+  // ];
+
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (todo !== '') {
+      setTodos([{id: `${todo}-${Date.now()}` ,todo }, ...todos])
     }
-  ];
+  }
 
   return (
     <div className="App">
@@ -30,16 +41,21 @@ const App = () => {
 
         <div className="container">
           <h1>Todo List App</h1>
-          <form className="todoForm">
-            <input type="text" />
-            <button>Go</button>
+          <form className="todoForm" onSubmit={handleSubmit}>
+            <input type="text" onChange={(e) => setTodo(e.target.value)} />
+            <button type="submit">Go</button>
           </form>
           <ul className="allTodos">
-            <li className="singleTodo">
-              <span className="todoText">Learn React</span>
-              <button>Edit</button>
-              <button>Delete</button>
-            </li>
+            {
+              todos.map((t) => (
+
+                <li className="singleTodo">
+                  <span className="todoText">{t.todo}</span>
+                  <button>Edit</button>
+                  <button>Delete</button>
+                </li>
+              ))
+            }
           </ul>
         </div>
       }
